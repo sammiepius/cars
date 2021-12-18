cb8273fac0f5acb167370e430867e46b8ec86ec4
sammiepius <sammiepius10@gmail.com>
2021-12-18 03:53:25 +0000

    added changes

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
+import StyleButton from '../StyleButton';
 import styles from './styles';
 
 function CarItem(props) {
   return (
     <View style={styles.carContainer}>
       <ImageBackground
-        source={require('../../app/assets/car3.jpeg  ')}
+        source={require('../../app/assets/car3.jpeg')}
         style={styles.images}
       />
       <View style={styles.titles}>
-        <Text style={styles.title}>Model</Text>
+        <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $70,000</Text>
       </View>
+      <StyleButton
+        type="primary"
+        content={'Custom Order'}
+        onPress={() => {
+          console.warn('custom order was pressed');
+        }}
+      />
+      <StyleButton
+        type="secondary"
+        content={'Existing Inventory'}
+        onPress={() => {
+          console.warn('existing was pressed');
+        }}
+      />
     </View>
   );
 }
---
+++ components/StyleButton/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, Pressable } from 'react-native';
+import styles from './styles';
+
+function StyleButton(props) {
+  const type = props.type;
+  const content = props.content;
+  const onPress = props.onPress;
+
+  const backgroundColor = type === 'primary' ? '#171A20CC' : '#FFFFFFA6';
+  const textColor = type === 'primary' ? '#FFFFFF' : '#171A20';
+  return (
+    <View style={styles.container}>
+      <Pressable
+        style={[styles.button, { backgroundColor: backgroundColor }]}
+        onPress={() => console.warn('pressed')}>
+        <Text style={([styles.text], { color: textColor })}>{content}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default StyleButton;
